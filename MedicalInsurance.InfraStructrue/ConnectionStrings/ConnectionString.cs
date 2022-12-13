a5570226dab6cc47bef30b988983ac8d291e1b40
MarjanNorouzi <marjan33norouzi@gmail.com>
2022-12-13 19:04:01 +0330

    initial project

---
+++ MedicalInsurance.InfraStructrue/ConnectionStrings/ConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalInsurance.InfraStructrue.ConnectionStrings
+{
+    public class ConnectionString
+    {
+
+    }
+}
